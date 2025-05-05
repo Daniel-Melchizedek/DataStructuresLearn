9a5ff19c03478f59693466c5f3554db283737150
Daniel Melchizedek Arockia Thomas Samuel <daniel.thomas.samuel@accenture.com>
2025-05-05 17:11:46 +0530

    learning

---
+++ Arrays_04_Rotate/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Arrays_04_Rotate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int d = 8;
+            int n = 4;
+            Console.WriteLine($"Rotate:{d} Size:{n}");
+            Console.WriteLine($"Reminder: {d%n}");
+        }
+    }
+}
